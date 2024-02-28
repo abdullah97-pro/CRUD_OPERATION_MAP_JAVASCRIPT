bd69d6258ba4ae452c767e751e0d20abb1e4dc23
abdullah97-pro <abdullahahmadi019@gmail.com>
2024-02-28 07:07:46 +0430

    get form values by id in javasript

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// function to insert data
+function insertData() {
+    // get form values
+    var title = document.getElementById('titleInput').value;
+    var paragraph = document.getElementById('paragraphInput').value;
+    var image = document.getElementById('imageInput').files[0];
+}
